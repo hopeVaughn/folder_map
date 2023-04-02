2030355f58c311aab98176f3d04c6fc9f87d64e6
Hope <hopevaughnwarren@gmail.com>
2023-04-02 13:15:15 -0700

    add styling to exclude component

--- src/components/ExcludeFolderInputProps.tsx
+++ src/components/ExcludeFolderInputProps.tsx
@@ -27,19 +27,30 @@ export const ExcludeFoldersInput: React.FC<ExcludeFoldersInputProps> = ({
   };
 
   return (
-    <div>
+    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4'>
       {foldersToExclude.map((folder, index) => (
-        <div key={index} className='flex items-center space-x-2'>
+        <div key={index} className='flex items-center space-x-2 my-2'>
           <input
             type='text'
             value={folder}
             placeholder={`Folder to exclude ${index + 1}`}
             onChange={(e) => handleFolderChange(index, e.target.value)}
+            className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
           />
-          <button onClick={() => handleRemoveFolder(index)}>Remove</button>
+          <button
+            onClick={() => handleRemoveFolder(index)}
+            className='bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600'
+          >
+            Remove
+          </button>
         </div>
       ))}
-      <button onClick={handleAddFolder}>Add Folder to Exclude</button>
+      <button
+        onClick={handleAddFolder}
+        className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600'
+      >
+        Add Folder to Exclude
+      </button>
     </div>
   );
 };
